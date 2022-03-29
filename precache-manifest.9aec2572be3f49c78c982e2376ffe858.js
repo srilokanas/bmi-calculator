a97cf14880ac88684c041844dccd95b331e1b9b0
srilokanas <srilokanas@gmail.com>
2022-03-29 18:05:18 +0530

    Updates

---
+++ precache-manifest.9aec2572be3f49c78c982e2376ffe858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868b0ad46a4de7312449d42888d494e4",
+    "url": "/srilokanas/bmi-calculator/index.html"
+  },
+  {
+    "revision": "9e69dfa449e708a110da",
+    "url": "/srilokanas/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "55cc72080964fc3b2967",
+    "url": "/srilokanas/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "9e69dfa449e708a110da",
+    "url": "/srilokanas/bmi-calculator/static/js/2.88fa7e6a.chunk.js"
+  },
+  {
+    "revision": "55cc72080964fc3b2967",
+    "url": "/srilokanas/bmi-calculator/static/js/main.a4e88274.chunk.js"
+  },
+  {
+    "revision": "a0b8706067cd3d2d77c4",
+    "url": "/srilokanas/bmi-calculator/static/js/runtime~main.5c0f51e8.js"
+  }
+]);
