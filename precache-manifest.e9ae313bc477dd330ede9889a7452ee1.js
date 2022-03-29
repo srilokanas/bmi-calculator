2dc849603da3547c8413fba1b9fd9f74376b3349
srilokanas <srilokanas@gmail.com>
2022-03-29 18:50:37 +0530

    Updates

---
+++ precache-manifest.e9ae313bc477dd330ede9889a7452ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828f4178f8ef00e5f19e4c0638dd182c",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b01293a29c996aa61943",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "7508008afea74785802e",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b01293a29c996aa61943",
+    "url": "/bmi-calculator/static/js/2.9a31836e.chunk.js"
+  },
+  {
+    "revision": "7508008afea74785802e",
+    "url": "/bmi-calculator/static/js/main.aab64653.chunk.js"
+  },
+  {
+    "revision": "13e581a05211d44f5a61",
+    "url": "/bmi-calculator/static/js/runtime~main.717d4b37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf5543548ecc5ef1cb0f1f4c18cf3354.js"
+  "/bmi-calculator/precache-manifest.e9ae313bc477dd330ede9889a7452ee1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bmi-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
